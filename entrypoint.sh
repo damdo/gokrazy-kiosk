774d0dffd063319c72985ff699b279a17002092c
Damiano Donati <damiano.donati@gmail.com>
2024-08-05 18:39:29 +0200

    continuously try to maximize browser window

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 if [[ $# -eq 0 ]] ; then
     /usr/bin/X :0 -nolisten tcp vt1
 else
-    # maximize the main window
-    echo 'nohup bash -c "sleep 10 ; xdotool getwindowfocus windowsize $(xdotool getdisplaygeometry)" &' > /usr/bin/entrypoint-command.sh
+    # keep maximizing the main window
+    echo 'nohup bash -c "while true; do xdotool getwindowfocus windowsize $(xdotool getdisplaygeometry) || true; sleep 20; done" &' > /usr/bin/entrypoint-command.sh
 
     echo "$@" >> /usr/bin/entrypoint-command.sh
     chmod +x /usr/bin/entrypoint-command.sh
