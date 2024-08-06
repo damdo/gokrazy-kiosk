706be8fb54fe61cdbcb4035890602fb270f0bd67
Damiano Donati <damiano.donati@gmail.com>
2024-08-06 14:55:44 +0200

    add 10s sleep before starting to maximize

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ if [[ $# -eq 0 ]] ; then
     /usr/bin/X :0 -nolisten tcp vt1
 else
     # keep maximizing the main window
-    echo 'nohup bash -c "while true; do xdotool getwindowfocus windowsize $(xdotool getdisplaygeometry) || true; sleep 20; done" &' > /usr/bin/entrypoint-command.sh
-
+    echo 'nohup bash -c "sleep 10; while true; do xdotool getwindowfocus windowsize $(xdotool getdisplaygeometry) || true; sleep 20; done" &' > /usr/bin/entrypoint-command.sh
     echo "$@" >> /usr/bin/entrypoint-command.sh
     chmod +x /usr/bin/entrypoint-command.sh
     /usr/bin/xinit /usr/bin/entrypoint-command.sh -- :0 -nolisten tcp vt1
